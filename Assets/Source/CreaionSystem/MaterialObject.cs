8c5045026da2807ec0dbd916e07a071eba27a3a7
SobolevicDaniell <sobolevdan99@yandex.ru>
2024-04-14 17:52:15 +0300

    Add main mechanic

--- Assets/Source/CreaionSystem/MaterialObject.cs
+++ Assets/Source/CreaionSystem/MaterialObject.cs
@@ -3,45 +3,128 @@ using UnityEngine;
 public class MaterialObject : MonoBehaviour
 {
     [SerializeField] private Materials _material;
-    [SerializeField] private Collider _table;
-    [SerializeField] private GameObject Material_Object;
+    [SerializeField] private GameObject _newObject;
+    
+    private Collider _table;
+    private bool _newObjectSpawned = false;
 
-    private void OnTriggerEnter(Collider other)
+    private void Start()
     {
-        if (other.CompareTag("MaterialObject") && other.GetComponent<MaterialObject>() != null && other.GetComponent<MaterialObject>()._table == _table)
+        _table = GameObject.FindWithTag("Table").GetComponent<Collider>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("MaterialObject") &&
+            other.gameObject.GetComponent<MaterialObject>() != null &&
+            _table.bounds.Contains(other.gameObject.transform.position) &&
+            other.gameObject.GetComponent<MaterialObject>()._table == _table)
         {
-            MaterialObject otherMaterialObject = other.GetComponent<MaterialObject>();
-
-            if ((_material == Materials.sulfur && otherMaterialObject._material == Materials.coal) ||
-                (_material == Materials.coal && otherMaterialObject._material == Materials.sulfur))
+            MaterialObject otherMaterialObject = other.gameObject.GetComponent<MaterialObject>();
+            if (!_newObjectSpawned && !otherMaterialObject._newObjectSpawned)
             {
-                Destroy(gameObject);
-                Destroy(other.gameObject);
-
-                // Проверяем, является ли текущий объект sulfur, чтобы вызвать SpawnNewObject только один раз
-                if (_material == Materials.sulfur)
+                if (CanCombine(_material, otherMaterialObject._material))
                 {
-                    SpawnNewObject(Material_Object);
+                    CreateNewObject(otherMaterialObject);
+                    _newObjectSpawned = true;
+                    otherMaterialObject._newObjectSpawned = true;
+                
+                    Destroy(gameObject);
+                    Destroy(other.gameObject);
                 }
             }
         }
     }
 
+    private bool CanCombine(Materials material1, Materials material2)
+    {
+        return (material1 == Materials.sulfur && material2 == Materials.coal) ||
+               (material1 == Materials.coal && material2 == Materials.sulfur) ||
+               (material1 == Materials.mercury && material2 == Materials.carrot) ||
+               (material1 == Materials.carrot && material2 == Materials.mercury) ||
+               (material1 == Materials.destructive_mix && material2 == Materials.leaf_tree_life) ||
+               (material1 == Materials.leaf_tree_life && material2 == Materials.destructive_mix) ||
+               (material1 == Materials.explosive_potion && material2 == Materials.fire_poison_potion) ||
+               (material1 == Materials.fire_poison_potion && material2 == Materials.explosive_potion) ||
+               (material1 == Materials.tornado_potion && material2 == Materials.fire_poison_potion) ||
+               (material1 == Materials.fire_poison_potion && material2 == Materials.tornado_potion) ||
+               (material1 == Materials.gap_potion && material2 == Materials.blue_flame_potion) ||
+               (material1 == Materials.blue_flame_potion && material2 == Materials.gap_potion) ||
+               (material1 == Materials.infernal_potion && material2 == Materials.heart_black_dragon) ||
+               (material1 == Materials.heart_black_dragon && material2 == Materials.infernal_potion);
+    }
 
-    private void SpawnNewObject(GameObject prefab)
+    private void CreateNewObject(MaterialObject otherMaterialObject)
+    {
+        Destroy(otherMaterialObject.gameObject);
+        SpawnNewObject(_newObject, GetCombinedMaterial(_material, otherMaterialObject._material));
+    }
+
+    private void SpawnNewObject(GameObject prefab, Materials materials)
     {
         GameObject newObject = Instantiate(prefab, transform.position, Quaternion.identity);
         MaterialObject newMaterialObject = newObject.GetComponent<MaterialObject>();
-        newMaterialObject._material = Materials.gunpowder;
+        newMaterialObject._material = materials;
         newMaterialObject._table = _table;
-        Destroy(gameObject);
     }
 
+    private Materials GetCombinedMaterial(Materials material1, Materials material2)
+    {
+        if ((material1 == Materials.sulfur && material2 == Materials.coal) ||
+            (material1 == Materials.coal && material2 == Materials.sulfur))
+        {
+            return Materials.explosive_potion;
+        }
+        else if ((material1 == Materials.mercury && material2 == Materials.carrot) ||
+                 (material1 == Materials.carrot && material2 == Materials.mercury))
+        {
+            return Materials.fire_poison_potion;
+        }
+        else if ((material1 == Materials.destructive_mix && material2 == Materials.leaf_tree_life) ||
+                 (material1 == Materials.leaf_tree_life && material2 == Materials.destructive_mix))
+        {
+            return Materials.tornado_potion;
+        }
+        else if ((material1 == Materials.explosive_potion && material2 == Materials.fire_poison_potion) ||
+                 (material1 == Materials.fire_poison_potion && material2 == Materials.explosive_potion))
+        {
+            return Materials.gap_potion;
+        }
+        else if ((material1 == Materials.tornado_potion && material2 == Materials.fire_poison_potion) ||
+                 (material1 == Materials.fire_poison_potion && material2 == Materials.tornado_potion))
+        {
+            return Materials.blue_flame_potion;
+        }
+        else if ((material1 == Materials.gap_potion && material2 == Materials.blue_flame_potion) ||
+                 (material1 == Materials.blue_flame_potion && material2 == Materials.gap_potion))
+        {
+            return Materials.infernal_potion;
+        }
+        else if ((material1 == Materials.infernal_potion && material2 == Materials.heart_black_dragon) ||
+                 (material1 == Materials.heart_black_dragon && material2 == Materials.infernal_potion))
+        {
+            return Materials.summoning_potion;
+        }
+        
+        return Materials.sulfur;
+        
+    }
 
     public enum Materials
     {
         sulfur,
         coal,
-        gunpowder
+        mercury,
+        carrot,
+        destructive_mix,
+        leaf_tree_life,
+        heart_black_dragon,
+        explosive_potion,
+        fire_poison_potion,
+        tornado_potion,
+        gap_potion,
+        blue_flame_potion,
+        infernal_potion,
+        summoning_potion
     }
 }
