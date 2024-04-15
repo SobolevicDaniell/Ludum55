6e17fa91fcd0b18bdece4c7d3cdf9c6e66123378
praprf <fil18mot@gmail.com>
2024-04-15 21:47:54 +0300

    changed book

--- Assets/Source/ItemSystem/BookSystem.cs
+++ Assets/Source/ItemSystem/BookSystem.cs
@@ -35,7 +35,7 @@ namespace ItemSystem
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
-            if (Physics.Raycast(ray, out hit, 5f))
+            if (Physics.Raycast(ray, out hit, 2f))
             {
                 // Убираем проверку на слой
                 // if ((_player.Book & (1 << hit.collider.gameObject.layer)) != 0)
