1123b39ae22d74bafb79b2c0751b839b2cbf216d
SobolevicDaniell <sobolevdan99@yandex.ru>
2024-04-15 17:02:49 +0300

    Add applause

--- Assets/Source/CellSystem/CellAndDemon.cs
+++ Assets/Source/CellSystem/CellAndDemon.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CellAndDemon : MonoBehaviour
 {
@@ -8,6 +10,17 @@ public class CellAndDemon : MonoBehaviour
 
     [SerializeField] private GameObject _demonSpawnPosition;
 
+    
+    [SerializeField] private AudioClip _groundImpactSound;
+    [SerializeField] private AudioClip _applause;
+    [SerializeField] private AudioClip _screams;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         MaterialObject materialObject = other.gameObject.GetComponent<MaterialObject>();
@@ -16,31 +29,38 @@ public class CellAndDemon : MonoBehaviour
             if (materialObject._material == MaterialObject.Materials.explosive_potion)
             {
                 Destroy(materialObject.gameObject);
+                Applauses();
                 SpawnCoin(6);
+                
             }
             else if (materialObject._material == MaterialObject.Materials.fire_poison_potion)
             {
                 Destroy(materialObject.gameObject);
+                Applauses();
                 SpawnCoin(9);
             }
             else if (materialObject._material == MaterialObject.Materials.tornado_potion)
             {
                 Destroy(materialObject.gameObject);
+                Applauses();
                 SpawnCoin(13);
             }
             else if (materialObject._material == MaterialObject.Materials.gap_potion)
             {
                 Destroy(materialObject.gameObject);
+                Applauses();
                 SpawnCoin(16);
             }
             else if (materialObject._material == MaterialObject.Materials.blue_flame_potion)
             {
                 Destroy(materialObject.gameObject);
+                Applauses();
                 SpawnCoin(26);
             }
             else if (materialObject._material == MaterialObject.Materials.infernal_potion)
             {
                 Destroy(materialObject.gameObject);
+                Applauses();
                 SpawnCoin(38);
             }
             else if (materialObject._material == MaterialObject.Materials.summoning_potion)
@@ -49,6 +69,17 @@ public class CellAndDemon : MonoBehaviour
                 SpawnDemon();
             }
         }
+    }
+
+    // private void CoinSound()
+    // {
+    //     _audioSource.PlayOneShot(_groundImpactSound);
+    // }
+
+    private void Applauses()
+    {
+        _audioSource.PlayOneShot(_applause);
+        _audioSource.PlayOneShot(_screams);
     }
 
     private void SpawnDemon()
