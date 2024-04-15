95cda229cdde22a5fa81e4398a507aa930bd59d1
praprf <fil18mot@gmail.com>
2024-04-15 18:20:25 +0300

    added main sound

--- Assets/Source/ItemSystem/BookSystem.cs
+++ Assets/Source/ItemSystem/BookSystem.cs
@@ -29,7 +29,7 @@ namespace ItemSystem
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
-            if (Physics.Raycast(ray, out hit, 2f))
+            if (Physics.Raycast(ray, out hit, 3f))
             {
                 if ((_player.Book & (1 << hit.collider.gameObject.layer)) != 0)
                 {
